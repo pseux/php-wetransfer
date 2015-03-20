330b4875409663ea5032927efec9590e5c0a91bd
osvenson <oliver@libertytech.com>
2015-03-20 12:36:10 +0000

    Converted inline code to function.

--- snippet.php
+++ snippet.php
@@ -1,22 +1,23 @@
 <?php
 
-$url = 'https://www.wetransfer.com/downloads/XXXXXXXX/YYY';
-$target = 'download.zip';
-
-// --
-
-$parts = explode('/', $url);
-$parts = array_slice($parts, -2);
-
-$first_url = vsprintf('https://www.wetransfer.com/api/v1/transfers/%s/download?recipient_id=&security_hash=%s&password=&ie=false', $parts);
-
-$response = file_get_contents($first_url);
-$response = json_decode($response, true);
-
-if ($second_url = arp($response, 'direct_link')); 
-	else die('Invalid URL (or WeTransfer changed things)');
+function downloadWeTransfer($url, $target)
+{
+	$parts = explode('/', $url);
+	$parts = array_slice($parts, -2);
 	
-// --
+	$virtual_link = vsprintf('https://www.wetransfer.com/api/v1/transfers/%s/download?recipient_id=&security_hash=%s&password=&ie=false', $parts);
+	
+	$response = file_get_contents($virtual_link);
+	$response = json_decode($response, true);
+	
+	if ($direct_link = arp($response, 'direct_link')); 
+		else throw new Exception('Invalid URL (or WeTransfer changed things)');
+		
+	if (file_put_contents($target, file_get_contents($direct_link)) === false)
+		throw new Exception('Error saving file.');
+}
 
-file_put_contents($target, file_get_contents($second_url));
+// -- Example:  
+
+downloadWeTransfer('https://www.wetransfer.com/downloads/XXXXXXXX/YYY', 'download.zip');
 
